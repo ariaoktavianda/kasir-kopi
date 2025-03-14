4b544ba03a0630249d3eeb5805443e641ecc7943
ariaoktavianda <oktaviandaria@gmil.com>
2025-03-14 22:43:25 +0700

    new

--- kasirkopi.cpp
+++ kasirkopi.cpp
@@ -1,15 +1,15 @@
 //latihan float
 #include <stdio.h>
+using namespace std;
 
 int main(){
     int jumblah_kopi;
     float harga_kopi=7000, total;
-    printf("maskan jumblah kopi yang dibeli: ");
+    printf("masukan jumblah kopi yang dibeli: ");
     scanf("%d", &jumblah_kopi);
 
     total= jumblah_kopi*harga_kopi;
     printf("total harga yang harus dibayar: Rp%.2f", total);
 
-    return 0; 
-    //ariaoktavianda.github.io
+    return 0;
 }
